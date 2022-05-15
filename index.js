f2dbba3a71e57474ef32f6475efa0b77b98d5644
cas1m <casimbabazade@gmail.com>
2022-05-15 18:24:43 +0400

    Website end

---
+++ index.js
@@ -0,0 +1,69 @@
+const swipe = new Swiper(".mySwiper", {
+    loop: true,
+    spaceBetween: 10,
+    centeredSlides: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },    
+  });
+
+  const counters = document.querySelectorAll(".count");
+  const speed = 300;
+  
+  counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = parseInt(+counter.getAttribute("data-target"));
+    const count = parseInt(+counter.innerText);
+    const increment = Math.trunc(target / speed);
+    console.log(increment);
+  
+    if (count < target) {
+      counter.innerText = count + increment;
+      setTimeout(updateCount, 1);
+    } else {
+      count.innerText = target;
+    }
+  };
+  updateCount();
+  });
+ 
+  
+  $('#notify-form').submit(function (event) {
+    event.preventDefault();
+       $('.notify-form').removeClass('active')
+       $('.message-end').addClass('active')
+ 
+       setTimeout( function (event) {
+         $('.message-end').removeClass('active')
+         $('.btn-send').removeClass('active')
+         $('.field input').val("")
+       }, 2000)
+   })
+ 
+     $('.message:not(.message-end)').click(function (event) {
+       $('.notify-form').addClass('active')
+       $('.field input').focus()
+     })
+ 
+     $('.field input').keyup(function(event) {
+ 
+       if (/^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,6}$/.test(event.target.value)) {
+         $('.btn-send').addClass('active')				
+       }
+       else {
+         $('.btn-send').removeClass('active')
+       }
+     })
+
+// let button = document.getElementById("button");
+// button.addEventListener('mousemove', (e) => {
+//     x = e.offsetX;
+//     y = e.offsetY;
+//     button.style.setProperty('--mouse-x', x + "px");
+//     button.style.setProperty('--mouse-y', y + "px");
+// });
