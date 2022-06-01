758f5f2dbb2b1196b6c254198c9301cf6cd546a9
cas1m <casimbabazade@gmail.com>
2022-06-01 16:36:41 +0400

    PortFolio

--- blog.js
+++ blog.js
@@ -31,19 +31,31 @@ const swipe = new Swiper(".mySwiper", {
   };
   updateCount();
   });
+ 
   
-  const bars = document.getElementById('bars');
-  const mobileMenu = document.getElementById('mobileMenu');
-  bars.addEventListener('click', function() {
-    mobileMenu.classList.toggle('active')
-  })
-
-
-
-// let button = document.getElementById("button");
-// button.addEventListener('mousemove', (e) => {
-//     x = e.offsetX;
-//     y = e.offsetY;
-//     button.style.setProperty('--mouse-x', x + "px");
-//     button.style.setProperty('--mouse-y', y + "px");
-// });
+  $('#notify-form').submit(function (event) {
+    event.preventDefault();
+       $('.notify-form').removeClass('active')
+       $('.message-end').addClass('active')
+ 
+       setTimeout( function (event) {
+         $('.message-end').removeClass('active')
+         $('.btn-send').removeClass('active')
+         $('.field input').val("")
+       }, 2000)
+   })
+ 
+     $('.message:not(.message-end)').click(function (event) {
+       $('.notify-form').addClass('active')
+       $('.field input').focus()
+     })
+ 
+     $('.field input').keyup(function(event) {
+ 
+       if (/^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,6}$/.test(event.target.value)) {
+         $('.btn-send').addClass('active')				
+       }
+       else {
+         $('.btn-send').removeClass('active')
+       }
+     })
