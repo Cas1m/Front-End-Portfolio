0e9e204675621152816ebe693d0dfe5d0f197355
cas1m <casimbabazade@gmail.com>
2022-07-24 20:39:11 +0400

    YOG

--- js/index.js
+++ js/index.js
@@ -1,70 +1,62 @@
-const swipe = new Swiper(".mySwiper", {
-    loop: true,
-    spaceBetween: 10,
-    centeredSlides: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },    
-  });
-
-  const counters = document.querySelectorAll(".count");
-  const speed = 300;
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 70,
+  slidesPerGroup: 4,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+  // const counters = document.querySelectorAll(".count");
+  // const speed = 300;
   
-  counters.forEach((counter) => {
-  const updateCount = () => {
-    const target = parseInt(+counter.getAttribute("data-target"));
-    const count = parseInt(+counter.innerText);
-    const increment = Math.trunc(target / speed);
+  // counters.forEach((counter) => {
+  // const updateCount = () => {
+  //   const target = parseInt(+counter.getAttribute("data-target"));
+  //   const count = parseInt(+counter.innerText);
+  //   const increment = Math.trunc(target / speed);
+  //   console.log(increment);
+  
+  //   if (count < target) {
+  //     counter.innerText = count + increment;
+  //     setTimeout(updateCount, 1);
+  //   } else {
+  //     count.innerText = target;
+  //   }
+  // };
+  // updateCount();
+  // });
  
+//   const bars = document.getElementById('bars');
+//   const mobileMenu = document.getElementById('mobileMenu');
+//   bars.addEventListener('click', function() {
+//     mobileMenu.classList.toggle('active')
+//   })
+
+
+//   const validateEmail = (email) => {
+//     return email.match(
+//       /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+//     );
+//   };
   
-    if (count < target) {
-      counter.innerText = count + increment;
-      setTimeout(updateCount, 1);
-    } else {
-      count.innerText = target;
-    }
-  };
-  updateCount();
-  });
-  
-  const bars = document.getElementById('bars');
-  const mobileMenu = document.getElementById('mobileMenu');
-  bars.addEventListener('click', function() {
-    mobileMenu.classList.toggle('active')
-  })
+// const validate = (selector) => {
+//   const result = document.querySelector('#result');
+//   const email = document.querySelector('#' + selector).value;
+//   result.textContent = "";
 
-
-  const validateEmail = (email) => {
-      return email.match(
-        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      );
-    };
-    
-  const validate = () => {
-      const result = document.querySelector('#result');
-      const email = document.querySelector('#email').value;
-      result.textContent = "";
-  
-      if (validateEmail(email)) {
-          result.textContent = email + ' is valid :)'
-          result.style.color = 'green';
-      } else {
-          result.textContent = email + ' is not valid :('
-          result.style.color = '#E77C40';
-      }
-      return false;
-  }
-
-
-// let button = document.getElementById("button");
-// button.addEventListener('mousemove', (e) => {
-//     x = e.offsetX;
-//     y = e.offsetY;
-//     button.style.setProperty('--mouse-x', x + "px");
-//     button.style.setProperty('--mouse-y', y + "px");
-// });
+//   if (validateEmail(email)) {
+//       result.textContent = email + ' is valid :)'
+//       result.style.color = 'green';
+//   } else {
+//       result.textContent = email + ' is not valid :('
+//       result.style.color = '#E77C40';
+//   }
+//   return false;
+// }
